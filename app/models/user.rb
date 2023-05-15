6e775b22c43d1132a009a1ed58aa43eff971d3ef
prototype2010 <alex.zakorko2015@gmail.com>
2023-05-15 10:33:16 +0300

    ERD, relations, user_id to test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :tests, dependent: :destroy
 
   def tests_by_level(level)
-    tests.where(id: results, level: level)
+    test_ids = results.pluck(:test_id)
+    tests.where(id: test_ids, level: level)
   end
 end
