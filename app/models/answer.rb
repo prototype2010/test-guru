c560697fd91b91194b96a9305fcaae275b8ee6e4
prototype2010 <alex.zakorko2015@gmail.com>
2023-05-20 14:13:08 +0300

    scopes, validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,16 +1,41 @@
 class Answer < ApplicationRecord
+  MAX_ANSWERS = 4
+  MIN_ANSWERS = 1
+
   belongs_to :question
 
-  validates_with AnswerValidators::AnswerMaximumCountValidator, on: :create
-  validates_with AnswerValidators::AnswerUpdateValidator, on: :update
+  validate :validate_maximum_answers_count, on: :create
+  validate :validate_answers_count, on: :update
 
-  before_destroy :check_remained_answers_count
-
-  scope :correct_only, -> { where(correct: true) }
+  scope :correct, -> { where(correct: true) }
 
   private
 
-  def check_remained_answers_count
-    throw :abort if question.answers.size - 1 < AnswerValidators::MIN_ANSWERS
+  def validate_maximum_answers_count
+    return unless question.answers.count >= 4
+
+    errors.add :answer, "Maximum answers count should not be more than #{MAX_ANSWERS}"
+  end
+
+  def validate_answers_count
+    @original_answer = Answer.find(id)
+
+    return if question_id == @original_answer.question_id
+
+    check_next_question_answers
+    answers_count = question.answers.count
+
+    return if answers_count > MIN_ANSWERS && answers_count < MAX_ANSWERS
+
+    answer.errors.add :answer, "Answers count should not be less than #{MAX_ANSWERS}" if answers_count - 1 < MIN_ANSWERS
+  end
+
+  def check_next_question_answers
+    next_answer_question = Question.find(question_id)
+    next_question_answers_count = next_answer_question.answers.count
+
+    return unless next_question_answers_count + 1 > MAX_ANSWERS
+
+    answer.errors.add :answer, "Answer with id #{next_answer_question.id} will have more than #{MAX_ANSWERS} allowed"
   end
 end
