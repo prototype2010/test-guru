ce804c86178eb422dabf6e0e72d2daa96e2af426
prototype2010 <alex.zakorko2015@gmail.com>
2023-06-15 18:40:49 +0300

    bootstrap ok

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def flash_class_by_type(flash_type)
+    case flash_type
+    when 'notice' then 'warning'
+    when 'alert' then 'danger'
+    else 'primary'
+    end
+  end
 end
