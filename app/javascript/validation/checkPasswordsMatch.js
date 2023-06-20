89ebca2918a30197b2e4d11dcad7480c6e77c0cd
prototype2010 <alex.zakorko2015@gmail.com>
2023-06-20 12:52:14 +0300

    convert script to class

--- app/javascript/validation/checkPasswordsMatch.js
+++ app/javascript/validation/checkPasswordsMatch.js
@@ -1,24 +1,38 @@
 document.addEventListener('turbolinks:load', () => {
-    if(document.querySelector('#new_user')) {
-        document
-            .querySelectorAll('#user_password, #user_password_confirmation')
-            .forEach(node => node.addEventListener('input', checkPasswordsMatch))
+    const form = document
+        .querySelector('#new_user')
+
+    if(form) {
+        new PasswordsMatchChecker(form).listenFormChange()
     }
 })
 
-const checkPasswordsMatch = () => {
-    const password = document.querySelector('#user_password');
-    const passwordConfirmation = document.querySelector('#user_password_confirmation');
-    const helperText = document.querySelector('#helper_text')
-
-    if(!passwordConfirmation.value) {
-        helperText.innerText = ''
-        return;
+class PasswordsMatchChecker {
+    constructor(form,
+                passwordId = '#user_password',
+                confirmationId = '#user_password_confirmation',
+                helperId = '#helper_text') {
+        this.form = form
+        this.password = form.querySelector(passwordId);
+        this.passwordConfirmation = form.querySelector(confirmationId);
+        this.helperText = form.querySelector(helperId);
     }
 
-    if(passwordConfirmation.value !== password.value) {
-        helperText.innerText = 'Passwords do not match'
-    } else {
-        helperText.innerText = ''
+    listenFormChange = () => {
+        this.form.addEventListener('input', this.validate)
+    }
+
+    validate = () => {
+        if(!this.passwordConfirmation.value) {
+            this.helperText.innerText = ''
+            return;
+        }
+
+        if(this.passwordConfirmation.value !== this.password.value) {
+            this.helperText.innerText = 'Passwords do not match'
+        } else {
+            this.helperText.innerText = ''
+        }
     }
 }
+
