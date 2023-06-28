70077650c3ad7365c5f5e54e119dd22a4ea0e2b9
prototype2010 <alex.zakorko2015@gmail.com>
2023-06-28 22:24:44 +0300

    timer

--- db/migrate/20230628163528_add_end_time_to_test_passage.rb
+++ db/migrate/20230628163528_add_end_time_to_test_passage.rb
@@ -1,5 +1,5 @@
 class AddEndTimeToTestPassage < ActiveRecord::Migration[6.1]
   def change
-    add_column :test_passages, :end_time, :datetime, null: false
+    add_column :test_passages, :end_time, :datetime, null: false, default: Time.now
   end
 end
