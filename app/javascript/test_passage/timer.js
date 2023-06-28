feda6b6ff76b3628ec7ff661213dc743430b2905
prototype2010 <alex.zakorko2015@gmail.com>
2023-06-28 22:20:49 +0300

    timer

---
+++ app/javascript/test_passage/timer.js
@@ -0,0 +1,51 @@
+document.addEventListener('turbolinks:load', () => {
+	checkTestTime();
+})
+
+function checkTestTime() {
+	const timerNode = document.getElementById('timer')
+
+	if(!timerNode) {
+		return;
+	}
+
+	const endTimeEncoded = document.cookie
+		.split("; ")
+		.find((row) => row.startsWith("end_time="))
+		?.split("=")[1];
+
+	if(!endTimeEncoded) {
+		return;
+	}
+
+	const endTimeString = decodeURIComponent(endTimeEncoded)
+	const endTime = new Date(endTimeString).getTime()
+
+	setInterval(() => {
+		if(isOutOfTime(endTime)) {
+			redirectToResult()
+		} else {
+			const currentTime = new Date().getTime()
+			timerNode.textContent = millisecondsToReadableFormat(endTime - currentTime)
+		}
+	}, 1000 )
+}
+
+function isOutOfTime(endTime) {
+	const currentTime = new Date().getTime()
+
+	return currentTime > endTime
+}
+
+function redirectToResult(){
+	const {pathname, search} = window.location
+
+	window.location.href = `${pathname}/result${search}`
+}
+
+function millisecondsToReadableFormat(ms) {
+	const minutes = Math.floor(ms / 1000 / 60)
+	const seconds = Math.floor( ms / 1000 % 60)
+
+	return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+}
---
+++ db/migrate/20230628163528_add_end_time_to_test_passage.rb
@@ -0,0 +1,5 @@
+class AddEndTimeToTestPassage < ActiveRecord::Migration[6.1]
+  def change
+    add_column :test_passages, :end_time, :datetime, null: false
+  end
+end
